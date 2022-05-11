5f34c802ebef84b1be437f5d551ed3a9a1cb90f8
Roger Rocha <roger.nizoli@gmail.com>
2022-05-11 19:06:56 -0300

    Adding typeorm

---
+++ src/database/migrations/1652294838824-CreateCategories.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateCategories1652294838824 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
