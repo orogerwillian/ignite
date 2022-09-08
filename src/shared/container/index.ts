9429a2f1997a50f98964be1fa6712686d1050683
Roger Rocha <roger.nizoli@gmail.com>
2022-09-08 01:21:24 -0300

    Upload Car Images

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -8,6 +8,8 @@ import {IUserRepository} from "@modules/accounts/repositories/IUserRepository";
 import {UsersRepository} from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import {ICarsRepository} from "@modules/cars/repositories/ICarsRepository";
 import {CarsRepository} from "@modules/cars/infra/typeorm/repositories/CarsRepository";
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -27,4 +29,9 @@ container.registerSingleton<IUserRepository>(
 container.registerSingleton<ICarsRepository>(
     "CarsRepository",
     CarsRepository
+);
+
+container.registerSingleton<ICarsImagesRepository>(
+    "CarsImagesRepository",
+    CarsImagesRepository
 );
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,32 +1,48 @@
-import {Router} from "express";
-import {CreateCarController} from "@modules/cars/useCases/car/create/CreateCarController";
+import { Router } from "express";
+import multer from "multer";
+
 import ensureAuthenticated from "@middlewares/ensureAuthenticated";
 import ensureAdmin from "@middlewares/ensureAdmin";
-import {ListAvailableCarsController} from "@modules/cars/useCases/car/listAvaliable/ListAvailableCarsController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/car/listAvaliable/ListAvailableCarsController";
 import {
-    CreateCarSpecificationController
+  CreateCarSpecificationController
 } from "@modules/cars/useCases/carSpecification/CreateCarSpecificationController";
+import { CreateCarController } from "@modules/cars/useCases/car/create/CreateCarController";
+import { UploadCarImagesController } from "@modules/cars/useCases/car/uploadImage/UploadCarImagesController";
+import uploadConfig from "@config/upload";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
+
+const upload = multer(uploadConfig.upload("./tmp/cars"));
 
 carsRoutes.post(
-    "/",
-    ensureAuthenticated,
-    ensureAdmin,
-    createCarController.handle
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle
 );
 
 carsRoutes.get("/available", listAvailableCarsController.handle);
 
 carsRoutes.post(
-    "/specifications/:id",
-    ensureAuthenticated,
-    ensureAdmin,
-    createCarSpecificationController.handle);
+  "/specifications/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarSpecificationController.handle
+);
+
+carsRoutes.post(
+  "/images/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  upload.array("images"),
+  uploadCarImagesController.handle
+);
 
 
-export {carsRoutes}
+export { carsRoutes };
